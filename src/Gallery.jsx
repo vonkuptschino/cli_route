0f20863111c61f1465a38cb749b2acba42ce1b8b
Sophia Kramar <>
2018-05-08 14:49:48 +0300

    no message

---
+++ src/Gallery.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Gallery = () =>
+	(<div><h1>Gallery</h1><p>Pics</p></div>)
+
+export default Gallery;
---
+++ src/Map.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Map = () =>
+	(<div><h1>Maaaaaap</h1><iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ac2c45209d7da548ac8450670f88dc64364c2830ab27d912a499514e48143c35d&amp;source=constructor" width="379" height="333" frameborder="0"></iframe></div>)
+
+export default Map;
